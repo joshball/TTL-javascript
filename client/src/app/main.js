127e5701d99a199b9b8f5e60c851e41facbead7b
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-01-29 18:36:31 -0800

    Adding base durandal client app

---
+++ client/src/app/main.js
@@ -0,0 +1,40 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../vendor/require/text',
+        'durandal':'../vendor/durandal/js',
+        'plugins' : '../vendor/durandal/js/plugins',
+        'transitions' : '../vendor/durandal/js/transitions',
+        'knockout': '../vendor/knockout/knockout-2.3.0',
+        'bootstrap': '../vendor/bootstrap/js/bootstrap',
+        'jquery': '../vendor/jquery/jquery-1.9.1'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+       }
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Storage Server';
+
+    app.configurePlugins({
+        router:true,
+        dialog: true,
+        widget: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
---
+++ client/src/app/viewmodels/overview.js
@@ -0,0 +1,9 @@
+﻿define(function(require) {
+    'use strict';
+    var ko = require('knockout');
+    var overviewVm = function () {
+        this.total =  ko.observable(window.ss.total);
+        this.used =  ko.observable(window.ss.used);
+    };
+    return overviewVm;
+});
